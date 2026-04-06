19c479ab085e94008140ac35c1097cf32b91bea6
SaanaValkama <saanavalkama@outlook.com>
2026-04-06 09:53:20 +0300

    sessions configured

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,6 +1,7 @@
 const DATABASE_URL = process.env.DATABASE_URL
 const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN
 const PORT = process.env.PORT
+const SESSION_SECRET = process.env.SESSION_SECRET
 
 if (!DATABASE_URL) {
   throw new Error("DATABASE_URL is missing")
@@ -14,9 +15,12 @@ if (!PORT) {
   throw new Error("PORT is missing")
 }
 
+if(!SESSION_SECRET) throw new Error("session secret missing")
+
 export const env = {
   DATABASE_URL,
   CLIENT_ORIGIN,
   PORT: Number(PORT),
+  SESSION_SECRET
 }
 
---
+++ server/src/middleware/sessions.ts
@@ -0,0 +1,24 @@
+import session from 'express-session'
+import pgSession from 'connect-pg-simple'
+import { env } from '../config/env.js'
+
+const pgStore = pgSession(session)
+
+export const sessionMiddleware = session({
+    store: new pgStore({
+        conString: env.DATABASE_URL,
+        tableName: 'user_session',
+        createTableIfMissing: true
+    }),
+    secret: env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized: false, 
+    cookie:{
+        httpOnly: true,
+        secure: false, //IN prod true
+        sameSite:'lax',
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+
+})
+
